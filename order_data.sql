6149aafedb5df1af1bb0ab7e02f0f5a679739ee3
Marko Vasiljevic <info.markovas@gmail.com>
2022-02-15 12:24:53 +0100

    Update order_data.sql

--- order_data.sql
+++ order_data.sql
@@ -2,7 +2,7 @@
 SELECT 'iid', order_number, COUNT(*) FROM order_data GROUP BY 'iid', order number HAVING COUNT(*) > 1; 
 
 /* Create new table without duplicates */
-CREATE TABLE order_data_no_duplicates AS SELECT DISTINCT * FROM order_data
+CREATE TABLE order_data_no_duplicates AS SELECT DISTINCT * FROM order_data;
 
 /* transform JSON data from order_data table */
 CREATE TABLE transformed_order_data (
@@ -13,13 +13,13 @@ CREATE TABLE transformed_order_data (
 	JSON_EXTRACT(orderlineitems_jsonb, '$[0].price.amount') AS product_price,
 	JSON_EXTRACT(orderlineitems_jsonb, '$[0].price.currency') AS product_price_currency
 	FROM order_data_no_duplicates
-	)
+);
 	
 /* Create a new column to sequentially rank the order withing each customer by chronological ORDER */
 SELECT customer_id, gender, CAST(SUBSTRING(order_created_at_utc, 1, 10) AS DATE) AS order_date,
 DENSE_RANK () OVER (PARTITION BY customer_id ORDER BY order_created_at_utc DESC) AS order_rank
 FROM order_data_no_duplicates
-ORDER BY order_rank DESC 
+ORDER BY order_rank DESC;
 
 /* Find out which group has the most members */
 SELECT COUNT(DISTINCT(customer_id)) AS num_of_members, group_name
@@ -27,18 +27,18 @@ FROM transformed_order_data
 GROUP BY 2
 HAVING LENGTH (group_name) > 0
 ORDER BY 1 DESC
-LIMIT 1
+LIMIT 1;
 
 /* Number of orders by country/state */
 SELECT customer_country, COUNT(customer_country)
 FROM transformed_order_data
 GROUP BY 1
-ORDER BY COUNT(1) DESC
+ORDER BY COUNT(1) DESC;
 
 /* Total Revenue in NY per month */
 SELECT MONTH(order_date) AS 'month', customer_state, SUM(order_total_amount) AS revenue
 FROM transformed_order_data
 GROUP BY MONTH(order_date), customer_state
-HAVING customer_state = 'NY'
+HAVING customer_state = 'NY';
 
 
